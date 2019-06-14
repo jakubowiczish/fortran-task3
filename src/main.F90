035bcc98344c1702316a6ceeea9d2188bb379a38
jakubowiczish <jakub77plotnikowski@gmail.com>
2019-06-14 15:47:56 +0200

    Add some basis in main.F90

--- src/main.F90
+++ src/main.F90
@@ -1,3 +1,26 @@
 program main
+    use trapezoidal_rule_integration
+
+    implicit none
+    real(kind = 8) :: ibeg
+    real(kind = 8) :: iend
+
+    procedure(fun_int), pointer :: my_fun
+
+    integer(kind = 8) :: p
+
+    real(kind = 8) :: sum
+
+    integer(kind = 8) :: i
+
+    real(kind = 8), codimension[*] :: result_sine
+
+    ibeg = -5.0
+    iend = 7.0
+    my_fun => sine
+
+
+
+
 
 end program
